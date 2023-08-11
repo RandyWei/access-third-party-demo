94d3b550e0892ca68563dd76f5daf5e11a59e84b
RandyWei <weizhengheng@chinahrt.com>
2023-08-11 09:26:40 +0800

    修改配置

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -17,17 +17,36 @@ kotlin {
 
 android {
     compileSdk = (findProperty("android.compileSdk") as String).toInt()
-    namespace = "com.myapplication"
+    namespace = "com.chinahrt.app.shitu"
 
     sourceSets["main"].manifest.srcFile("src/androidMain/AndroidManifest.xml")
 
     defaultConfig {
-        applicationId = "com.myapplication.MyApplication"
+        applicationId = "com.chinahrt.app.shitu"
         minSdk = (findProperty("android.minSdk") as String).toInt()
         targetSdk = (findProperty("android.targetSdk") as String).toInt()
         versionCode = 1
         versionName = "1.0"
     }
+
+    signingConfigs{
+        create("release"){
+            keyAlias = "key0"
+            keyPassword = "chinahrt00"
+            storePassword = "chinahrt00"
+            storeFile = file("/Users/wei/AndroidStudioProjects/shitu/android/app/keystore")
+        }
+    }
+
+    buildTypes {
+        getByName("release"){
+            signingConfig = signingConfigs.getByName("release")
+        }
+        getByName("debug"){
+            signingConfig = signingConfigs.getByName("release")
+        }
+    }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,4 @@
-rootProject.name = "MyApplication"
+rootProject.name = "ThirdPartyApp"
 
 include(":androidApp")
 include(":shared")
