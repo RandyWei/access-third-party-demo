2d6a91f5f98779939d0da79476d2d0ddeea3e898
RandyWei <weizhengheng@chinahrt.com>
2023-08-11 09:55:48 +0800

    增加安卓微信open sdk

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -47,6 +47,8 @@ kotlin {
                 api("androidx.activity:activity-compose:1.6.1")
                 api("androidx.appcompat:appcompat:1.6.1")
                 api("androidx.core:core-ktx:1.9.0")
+
+                api("com.tencent.mm.opensdk:wechat-sdk-android:+")
             }
         }
         val iosX64Main by getting
